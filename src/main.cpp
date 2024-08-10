9cbfadc6c7af395ba4725f8d1284e8eb5ffd4264
thiagsv <86622333+thiagsv@users.noreply.github.com>
2024-08-10 13:08:19 -0300

    feat: add processors tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,38 @@
+#include <Arduino.h>
+#include "tasks.h"
 
+TaskHandle_t task1Handle = NULL;
+TaskHandle_t task2Handle = NULL;
+
+int runCollect = 0; 
+QueueHandle_t imuDataQueue;
+
+void setup() {
+  Serial.begin(115200);
+
+  imuDataQueue = xQueueCreate(UINT32_MAX, sizeof(String));
+
+  xTaskCreatePinnedToCore(
+    Task1,
+    "Task1",
+    8192,
+    NULL,
+    1,  
+    &task1Handle,
+    0
+  );
+
+  xTaskCreatePinnedToCore(
+    Task2,
+    "Task2",
+    16384,
+    NULL,
+    2,
+    &task2Handle,
+    1
+  );
+}
+
+void loop() {
+
+}
