d9d58460d1b314e5356e216ac2af5ae25af95cc1
thiagsv <86622333+thiagsv@users.noreply.github.com>
2024-08-17 16:23:51 -0300

    feat: add libraries

--- include/tasks.h
+++ include/tasks.h
@@ -6,8 +6,21 @@
 #include <freertos/queue.h>
 #include <SD.h>
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
 #include <Wire.h>
+
+#include <FS.h>
+#include <FSImpl.h>
+#include <vfs_api.h>
+
+#include <AsyncEventSource.h>
+#include <AsyncJson.h>
+#include <AsyncWebSocket.h>
+#include <AsyncWebSynchronization.h>
+#include <ESPAsyncWebServer.h>
+#include <StringArray.h>
+#include <WebAuthentication.h>
+#include <WebHandlerImpl.h>
+#include <WebResponseImpl.h>
 
 extern int runCollect;
 extern QueueHandle_t imuDataQueue;
