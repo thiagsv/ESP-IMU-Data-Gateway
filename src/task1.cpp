d9ece98fbbeca5f408a6523d4b729ee4fe44ba73
thiagsv <86622333+thiagsv@users.noreply.github.com>
2024-08-20 19:51:07 -0300

    feat: adjust file writing to imudata struct

--- src/task1.cpp
+++ src/task1.cpp
@@ -5,15 +5,24 @@ void Task1(void *pvParameters) {
         if (runCollect) {
             String imuData;
             if (xQueueReceive(imuDataQueue, &imuData, 0) == pdPASS) {
-                File file = SD.open("/data.txt", FILE_APPEND);
+                File file = SD.open(fileName, FILE_APPEND);
                 if (file) {
-                    file.println(imuData);
+                    // Escreve a struct no arquivo
+                    file.print(imuData.AcX); file.print(",");
+                    file.print(imuData.AcY); file.print(",");
+                    file.print(imuData.AcZ); file.print(",");
+                    file.print(imuData.GyX); file.print(",");
+                    file.print(imuData.GyY); file.print(",");
+                    file.print(imuData.GyZ); file.print(",");
+                    file.println(imuData.Timestamp); file.print(";");
                     file.close();
-                    Serial.println("Dados escritos no SD: " + imuData);
+                    Serial.println("Dados escritos no SD.");
                 } else {
                     Serial.println("Falha ao abrir o arquivo no SD.");
                 }
             }
+        } else {
+            imuDataQueue = xQueueCreate(10, sizeof(String));
         }
         vTaskDelay(pdMS_TO_TICKS(500));
     }
