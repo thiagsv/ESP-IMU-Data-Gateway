5dac8b3370d77257fff861b874ae7a5faf538ba8
thiagsv <86622333+thiagsv@users.noreply.github.com>
2024-08-20 19:51:21 -0300

    chore: remove comment

--- src/task1.cpp
+++ src/task1.cpp
@@ -7,7 +7,6 @@ void Task1(void *pvParameters) {
             if (xQueueReceive(imuDataQueue, &imuData, 0) == pdPASS) {
                 File file = SD.open(fileName, FILE_APPEND);
                 if (file) {
-                    // Escreve a struct no arquivo
                     file.print(imuData.AcX); file.print(",");
                     file.print(imuData.AcY); file.print(",");
                     file.print(imuData.AcZ); file.print(",");
