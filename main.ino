085052e0500619208a7dd86e96f222e6dc5bc895
thiagsv <thiagogabriel2901@gmail.com>
2024-09-28 11:04:22 -0300

    feat: add mocking to test

--- main.ino
+++ main.ino
@@ -8,48 +8,48 @@ AsyncWebServer server(80);
 int runCollect = 0; 
 const char *ssid = "ESP32_AP";
 const char *password = "123456789";
-String fileName = "imuData.txt";
+String fileName = "/imuData.txt";
 
 void setup() {
   Serial.begin(115200);
-
+  delay(1000);
+  // Montar SPIFFS
   if (!SPIFFS.begin(true)) {
       Serial.println("Falha ao montar o sistema de arquivos SPIFFS.");
   } else {
       Serial.println("SPIFFS montado com sucesso.");
   }
 
+  // Criar dados mock (ou continuar a escrever no arquivo existente)
+  createMockIMUData();
+
+  // Criar ponto de acesso Wi-Fi
   WiFi.softAP(ssid, password);
   Serial.println("Ponto de acesso criado");
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
 
-  server.on("/toggle", HTTP_GET, [](AsyncWebServerRequest *request) {
+  // Endpoint para inverter o valor de runCollect
+  server.on("/toggle", HTTP_POST, [](AsyncWebServerRequest *request) {
       runCollect = !runCollect;
-      if (runCollect) {
-        initialTime = millis();
-        fileName = "/" + String(initialTime) + ".txt";
-      }
-      request->send(200, "text/plain", String(runCollect));
       Serial.println("Valor de runCollect invertido para: " + String(runCollect));
+      request->send(200, "text/plain", String(runCollect));
   });
 
+  // Endpoint para obter dados do arquivo
   server.on("/getData", HTTP_GET, [](AsyncWebServerRequest *request) {
       if (runCollect == 0) {
-          File file = SPIFFS.open("/" + fileName, "r");
+          File file = SPIFFS.open(fileName, "r");
   
           if (file) {
-              // Enviar o arquivo como um stream assíncrono
               request->send(file, fileName, "text/plain", true); 
-              // 'true' habilita o envio em chunks assíncronos
           } else {
-              request->send(500, "text/plain", "Failed to open file on SPIFFS");
+              request->send(500, "text/plain", "Falha ao abrir o arquivo no SPIFFS");
           }
       } else {
-          request->send(403, "text/plain", "Data collection is in progress. Try again later.");
+          request->send(403, "text/plain", "Coleta de dados em andamento. Tente mais tarde.");
       }
   });
-
 
   server.begin();
 
@@ -58,7 +58,7 @@ void setup() {
   xTaskCreatePinnedToCore(
     Task1,
     "Task1",
-    8192,
+    4096,
     NULL,
     1,
     &task1Handle,
@@ -68,7 +68,7 @@ void setup() {
   xTaskCreatePinnedToCore(
     Task2,
     "Task2",
-    16384,
+    8192,
     NULL,
     2,
     &task2Handle,
@@ -77,3 +77,31 @@ void setup() {
 }
 
 void loop() {}
+
+// Função para criar/atualizar o arquivo imuData.txt com dados mock
+void createMockIMUData() {
+  // Gera um arquivo mock com dados do IMU no SPIFFS no modo WRITE (sobrescrevendo o arquivo existente)
+  File file = SPIFFS.open(fileName, FILE_WRITE);  // Usando FILE_WRITE para sobrescrever
+
+  if (!file) {
+      Serial.println("Falha ao abrir o arquivo imuData.txt para escrita.");
+      return;
+  }
+
+  // Dados mock (AcX, AcY, AcZ, GyX, GyY, GyZ, Timestamp)
+  for (int i = 0; i < 10; i++) {
+      file.print(i); file.print(","); // AcX
+      file.print(i); file.print(","); // AcY
+      file.print(i); file.print(","); // AcZ
+      file.print(i); file.print(","); // GyX
+      file.print(i); file.print(","); // GyY
+      file.print(i); file.print(","); // GyZ
+      file.print(millis());  // Timestamp
+      file.print(";");     // Adiciona ";" no final da linha e nova linha
+  }
+
+  // Fecha o arquivo
+  file.close();
+  Serial.println("Dados mock sobrescritos no arquivo imuData.txt.");
+
+}
